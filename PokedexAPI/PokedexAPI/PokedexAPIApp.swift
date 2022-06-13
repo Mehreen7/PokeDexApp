76d6481126da152d1549dfeaf4f14311510eb050
Mehreen7 <45670632+Mehreen7@users.noreply.github.com>
2022-06-13 15:47:25 +0200

    First commit

---
+++ PokedexAPI/PokedexAPI/PokedexAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAPIApp.swift
+//  PokedexAPI
+//
+//  Created by Mehreen on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
